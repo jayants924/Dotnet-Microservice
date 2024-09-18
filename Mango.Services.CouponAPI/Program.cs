e81ccf1a2bc2a95ef4a1dc978df6e6a4570a4633
jayants924 <jayant@shorthills.ai>
2024-09-18 14:38:28 +0530

    Seeding data in Coupon tables

--- Mango.Services.CouponAPI/Program.cs
+++ Mango.Services.CouponAPI/Program.cs
@@ -27,4 +27,20 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
+
 app.Run();
+
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0) 
+        { 
+            _db.Database.Migrate();
+        }
+    }
+}
